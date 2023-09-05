08f49be0d7a51efab88d5cf0b2d7a09cc831ba3c
Alice Déchevaux <alicedechevaux@gmail.com>
2023-09-05 17:34:49 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/*
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			alignment: 'center',
+			detach: false
+		});
+
+	// Nav.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo h1').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
+
+// Dynamically generate minimum and maximum accepted dates for reservation date picker
+
+let today = new Date().toISOString().split("T")[0];
+let max = new Date(new Date().setDate(new Date().getDate() + 60)).toISOString().split("T")[0];
+document.getElementsByName("reservation-date")[0].setAttribute("min", today);
+document.getElementsByName("reservation-date")[0].value = today;
+document.getElementsByName("reservation-date")[0].setAttribute("max", max);
+console.log(limit)
